587bb1de9a69f591c8c5a799025c8308287d840f
Hari Vijay <smarther@Smarthers-Mac-mini.local>
2019-01-11 13:23:25 +0530

    Initial Commit

---
+++ sample-design/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample-design
+//
+//  Created by Smarther on 21/10/1940 Saka.
+//  Copyright © 1940 Sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ sample-designTests/sample_designTests.swift
@@ -0,0 +1,34 @@
+//
+//  sample_designTests.swift
+//  sample-designTests
+//
+//  Created by Smarther on 21/10/1940 Saka.
+//  Copyright © 1940 Sample. All rights reserved.
+//
+
+import XCTest
+@testable import sample_design
+
+class sample_designTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
